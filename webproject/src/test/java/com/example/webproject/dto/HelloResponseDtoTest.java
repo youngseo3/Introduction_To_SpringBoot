5f634a3210ebea4623d56d650f804d0211cc5ed6
youngseo3 <wys4475@gmail.com>
2024-08-21 17:22:32 +0900

    [#3]Feat: HelloResponseDto와 테스트 코드 작성

---
+++ webproject/src/test/java/com/example/webproject/dto/HelloResponseDtoTest.java
@@ -0,0 +1,22 @@
+package com.example.webproject.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HelloResponseDtoTest {
+
+    @Test
+    public void lombokTest() {
+        // given
+        String name = "test";
+        int amount = 1000;
+
+        // when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
