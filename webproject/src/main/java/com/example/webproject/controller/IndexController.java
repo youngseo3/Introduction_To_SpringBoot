ef040a482b676c4a623964a9e8e2adcaa4f05b47
youngseo3 <wys4475@gmail.com>
2024-09-04 19:29:26 +0900

    [#15]Chore: IndexController 파일 경로 변경

--- webproject/src/main/java/com/example/webproject/controller/IndexController.java
+++ webproject/src/main/java/com/example/webproject/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.example.webproject;
+package com.example.webproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
