6b97ffbe4b266ff6266ccc36cbe8fa10b058e7b3
youngseo3 <wys4475@gmail.com>
2024-09-07 21:45:06 +0900

    [#15]Feat: 게시글 조회 API 수정 findAllDesc 메소드 작성

--- webproject/src/main/java/com/example/webproject/controller/IndexController.java
+++ webproject/src/main/java/com/example/webproject/controller/IndexController.java
@@ -1,13 +1,19 @@
 package com.example.webproject.controller;
 
+import com.example.webproject.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    @GetMapping
-    public String index() {
+    private final PostsService postsService;
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
--- webproject/src/main/java/com/example/webproject/service/posts/PostsService.java
+++ webproject/src/main/java/com/example/webproject/service/posts/PostsService.java
@@ -1,13 +1,17 @@
 package com.example.webproject.service.posts;
 
 import com.example.webproject.domain.posts.Posts;
+import com.example.webproject.dto.PostsListResponseDto;
 import com.example.webproject.dto.PostsResponseDto;
 import com.example.webproject.dto.PostsSaveRequestDto;
 import com.example.webproject.dto.PostsUpdateRequestDto;
 import com.example.webproject.repository.PostsRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+// Transactional import 에러 수정
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,12 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
